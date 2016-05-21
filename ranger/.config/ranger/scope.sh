05319e30f92b721f2685c48596d8429ec4b5ae20
krompus <b.adam.martin@gmail.com>
2016-05-21 17:18:00 -0400

    ...

--- ranger/.config/ranger/scope.sh
+++ ranger/.config/ranger/scope.sh
@@ -92,6 +92,7 @@ case "$mimetype" in
     # Syntax highlight for text files:
     text/* | */xml)
         #try safepipe highlight --out-format=ansi "$path" && { dump | trim; exit 5; }
+        try safepipe highlight --style=candy --out-format=xterm256 "$path" && { dump | trim; exit 5; }
         try pygmentize -f 256 -O style=monokai "$path" && { dump | trim; exit 5; }
         #try safepipe pygmentize "$path" && { dump | trim; exit 5; }
         exit 2;;
